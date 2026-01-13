9e71ba9f5f2698c8dff5b92313fddaaabf1fdc1f
NOTEDELLANA\anap <de.anapaulasilva@gmail.com>
2026-01-13 14:39:12 -0300

    cursoIA update

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,8 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import paginaCursoIA from "@/public/paginaCursoIA.png";
+import paginaCursoIA from "@/public/cursoIA.png";
 import todososPuzzles from "@/public/todosPuzzles.png";
-import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
